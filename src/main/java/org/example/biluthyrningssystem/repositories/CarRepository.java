9276fb9ba6cc23d93c867df87c83479a6c28c03b
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-04-22 21:16:17 +0200

    Added attributes to Car class. Added extension to JpaRepository in CarRepository class.

--- src/main/java/org/example/biluthyrningssystem/repositories/CarRepository.java
+++ src/main/java/org/example/biluthyrningssystem/repositories/CarRepository.java
@@ -1,4 +1,12 @@
 package org.example.biluthyrningssystem.repositories;
 
-public interface CarRepository {
+
+import org.example.biluthyrningssystem.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Ann-Louis made this class
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
 }
