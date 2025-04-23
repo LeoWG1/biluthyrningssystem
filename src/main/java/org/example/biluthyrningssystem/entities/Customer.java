c540977de442526c4d5d68911da1aa478c51d808
LeoWG1 <leo.gestblom@gmail.com>
2025-04-23 11:16:56 +0200

    Completed Customer and CustomerRepository class

--- src/main/java/org/example/biluthyrningssystem/entities/Customer.java
+++ src/main/java/org/example/biluthyrningssystem/entities/Customer.java
@@ -1,4 +1,81 @@
 package org.example.biluthyrningssystem.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    private long id;
+    @Column(length = 13, unique=true, nullable=false)
+    private String socialSecurityNumber;
+    @Column(length = 30, nullable=false)
+    private String firstName;
+    @Column(length = 30, nullable=false)
+    private String lastName;
+    @Column(length = 30)
+    private String email;
+    @Column(length = 30)
+    private String address;
+    @Column(length = 30)
+    private String phoneNumber;
+
+    public Customer(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/main/java/org/example/biluthyrningssystem/repositories/CustomerRepository.java
+++ src/main/java/org/example/biluthyrningssystem/repositories/CustomerRepository.java
@@ -1,4 +1,7 @@
 package org.example.biluthyrningssystem.repositories;
 
-public interface CustomerRepository {
+import org.example.biluthyrningssystem.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
