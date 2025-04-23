ece78c096102743f840db57456a5b1fcdf3eaecc
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-04-23 20:41:10 +0200

    Added methods to CarServiceInterface.

--- src/main/java/org/example/biluthyrningssystem/services/CarServiceInterface.java
+++ src/main/java/org/example/biluthyrningssystem/services/CarServiceInterface.java
@@ -1,4 +1,16 @@
 package org.example.biluthyrningssystem.services;
 
+import org.example.biluthyrningssystem.entities.Car;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface CarServiceInterface {
+
+    ResponseEntity<List<Car>> getCars();
+    ResponseEntity<List<Car>> getAllCars();
+    ResponseEntity<Car> addCar(Car car);
+    ResponseEntity<Car> updateCar(Car car);
+    ResponseEntity<Car> removeCar(Car car);
+
 }
