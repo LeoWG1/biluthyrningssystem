01a6a2e2c59d3b678f5cea861d016e546655c95a
Frida Jakobsson <frja1700@student.miun.se>
2025-04-29 13:02:11 +0200

    basic statistics implemented

--- src/main/java/org/example/biluthyrningssystem/repositories/OrderRepository.java
+++ src/main/java/org/example/biluthyrningssystem/repositories/OrderRepository.java
@@ -2,6 +2,8 @@ package org.example.biluthyrningssystem.repositories;
 
 import org.example.biluthyrningssystem.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -12,6 +14,13 @@ import java.util.List;
  */
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o WHERE o.car.id = :carId AND o.active = true AND " +
+            "(o.startDate <= :endDate AND o.endDate >= :startDate)")
+    List<Order> findActiveOrdersWithin(
+            @Param("carId") long carId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
     List<Order> findByEndDateBefore(LocalDate endDate);
     List<Order> findByActiveTrue();
     List<Order> findByActiveFalse();
