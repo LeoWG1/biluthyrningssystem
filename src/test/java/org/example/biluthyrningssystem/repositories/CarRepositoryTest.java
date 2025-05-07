e3a218ed5e49156f963ecd23c7300d0163d8bae4
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-05-07 12:00:07 +0200

    Working on integrationtest

--- src/test/java/org/example/biluthyrningssystem/repositories/CarRepositoryTest.java
+++ src/test/java/org/example/biluthyrningssystem/repositories/CarRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+//Made by Ann-Louis
 @DataJpaTest
 class CarRepositoryTest {
 
--- src/test/java/org/example/biluthyrningssystem/services/CarServiceTest.java
+++ src/test/java/org/example/biluthyrningssystem/services/CarServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//Made by Ann-Louis
 @SpringBootTest
 class CarServiceTest {
 
