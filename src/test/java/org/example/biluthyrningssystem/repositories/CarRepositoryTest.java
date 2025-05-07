ecb0d7591deb2807fdceb0223785ce915b1a116f
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-05-07 12:12:56 +0200

    Updated with newest main

--- src/test/java/org/example/biluthyrningssystem/repositories/CarRepositoryTest.java
+++ src/test/java/org/example/biluthyrningssystem/repositories/CarRepositoryTest.java
@@ -1,12 +1,11 @@
 package org.example.biluthyrningssystem.repositories;
 
-import org.example.biluthyrningssystem.entities.Car;
+import org.example.biluthyrningssystem.models.entities.Car;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 //Made by Ann-Louis
 @DataJpaTest
